090502cf9ce93d67254a46d21a5cd8109c11eb09
kururu <rui.kondo@gmail.com>
2025-06-25 18:34:27 +0900

    noteサムネイル追加

--- api/note01.php
+++ api/note01.php
@@ -1,18 +1,13 @@
 <?php
-// RSSフィードのURL
 $rss_url = "https://note.com/kururu01/m/m417488290466/rss";
-
-// RSSを取得
 $rss_content = @file_get_contents($rss_url);
 
-// エラー処理
 if ($rss_content === false) {
     header('Content-Type: application/json');
     echo json_encode(['error' => 'RSS取得に失敗しました']);
     exit;
 }
 
-// XMLをパース
 $xml = simplexml_load_string($rss_content);
 
 if (!$xml || !isset($xml->channel->item[0])) {
@@ -21,18 +16,25 @@ if (!$xml || !isset($xml->channel->item[0])) {
     exit;
 }
 
-// 最新の記事を取得
+// media名前空間の登録
+$namespaces = $xml->channel->item[0]->getNamespaces(true);
+$media = $xml->channel->item[0]->children($namespaces['media'] ?? '');
+
+$thumbnailUrl = '';
+if (isset($media->thumbnail)) {
+    $thumbnailUrl = (string)$media->thumbnail->attributes()->url;
+}
+
 $latest = $xml->channel->item[0];
 
-// 必要な情報を抽出
 $data = [
     'title'       => (string)$latest->title,
     'link'        => (string)$latest->link,
     'description' => (string)$latest->description,
     'pubDate'     => (string)$latest->pubDate,
+    'thumbnail'   => $thumbnailUrl
 ];
 
-// JSONとして出力
 header('Content-Type: application/json; charset=utf-8');
 echo json_encode($data, JSON_UNESCAPED_UNICODE | JSON_PRETTY_PRINT);
 ?>
