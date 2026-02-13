c3a4cfb5e13fe464c98c4af33c0b78df5a794679
ruikondo <rui.kondo@gmail.com>
2026-02-13 17:35:51 +0900

    newsAPI02a

--- api/news01.php
+++ api/news01.php
@@ -1,42 +1,16 @@
 <?php
 $apiKey = getenv('NEWS_API');
-if (!$apiKey) {
-    http_response_code(500);
-    exit('API key not set');
-}
 
-// NewsAPI（BBCの記事を含む例）
 $url = 'https://newsapi.org/v2/top-headlines?' . http_build_query([
     'sources' => 'bbc-news',
     'apiKey'  => $apiKey,
 ]);
 
-$ch = curl_init($url);
-curl_setopt_array($ch, [
-    CURLOPT_RETURNTRANSFER => true,
-    CURLOPT_TIMEOUT => 5,
-]);
-
-$response = curl_exec($ch);
-curl_close($ch);
-
+$response = file_get_contents($url);
 $data = json_decode($response, true);
 
-if (!isset($data['articles'])) {
-    http_response_code(500);
-    exit('Invalid API response');
-}
-
-// 必要な項目だけ抽出
-$result = [];
 foreach ($data['articles'] as $article) {
-    $result[] = [
-        'title' => $article['title'] ?? '',
-        'thumbnail' => $article['urlToImage'] ?? null,
-    ];
+    echo $article['title'] . PHP_EOL;
+    echo $article['urlToImage'] . PHP_EOL;
 }
-
-// JSONで返す（スマホガジェット向け）
-header('Content-Type: application/json; charset=utf-8');
-echo json_encode($result, JSON_UNESCAPED_UNICODE);
 ?>
