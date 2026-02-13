b9b47718a96e128f8e9e152611ae5ed276c212c1
ruikondo <rui.kondo@gmail.com>
2026-02-13 17:52:54 +0900

    newsAPI02c

--- api/news01.php
+++ api/news01.php
@@ -15,7 +15,19 @@ $context = stream_context_create([
     ]
 ]);
 
-$response = file_get_contents($url, false, $context);
-var_dump($http_response_header);
-echo $response;
+$data = file_get_contents($url, false, $context);
+$article = $data['articles'][0] ?? null;
+
+if (!$article) {
+    echo json_encode(['error' => 'no articles'], JSON_UNESCAPED_UNICODE);
+    exit;
+}
+
+$output = [
+    'title'     => $article['title'] ?? '',
+    'thumbnail' => $article['urlToImage'] ?? '',
+];
+
+header('Content-Type: application/json; charset=utf-8');
+echo json_encode($output, JSON_UNESCAPED_UNICODE | JSON_UNESCAPED_SLASHES);
 ?>
